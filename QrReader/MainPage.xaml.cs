88f1ba8d1913d1ed8a34019ffc76b14e023faa08
jhoncabrera <jhoncabrera2050@gmail.com>
2023-05-21 18:59:36 -0500

    Agregar archivos de proyecto.

---
+++ QrReader/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using ZXing;
+using ZXing.Net.Mobile.Forms;
+
+namespace QrReader
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnScan_Clicked(object sender, EventArgs e)
+        {
+            Scanner();
+        }
+
+        private async void Scanner()
+        {
+            var scannerPage = new ZXingScannerPage();
+
+            scannerPage.Title = "QR reader";
+            scannerPage.OnScanResult += (result) =>
+            {
+                scannerPage.IsScanning = false;
+                Device.BeginInvokeOnMainThread(() =>
+                {
+                    Navigation.PopAsync();
+                    DisplayAlert("Obtained value", result.Text, "OK");
+                });
+            };
+
+            await Navigation.PushAsync(scannerPage);
+        }
+    }
+}
